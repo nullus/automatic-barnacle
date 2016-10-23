e739bcd9d629e5c5f3b8b81ef518135e187699b3
[null] <null@disasterarea.net>
2016-10-23 18:12:23 +1100

    Allow recursive searching (dangerous)

--- barnacles/data.py
+++ barnacles/data.py
@@ -4,7 +4,8 @@ import sys
 
 def get_files():
     return [
-        i.decode(sys.getfilesystemencoding())
-        for i in os.walk(os.getcwd()).next()[2]
+        os.path.relpath(os.path.join(walk[0], i), os.getcwd()).decode(sys.getfilesystemencoding())
+        for walk in os.walk(os.getcwd())
+        for i in walk[2]
         if i.lower().endswith('.mp4') or i.lower().endswith('.m4v')
     ]
