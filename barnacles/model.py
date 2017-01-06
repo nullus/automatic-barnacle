2b959046fde7576f3e7850c3d2f43a314623c9eb
[null] <null@disasterarea.net>
2017-01-06 23:00:18 +1100

    Update some models, and add a first migration with alembic

--- barnacles/model.py
+++ barnacles/model.py
@@ -4,6 +4,8 @@ from sqlalchemy.ext.declarative import declarative_base, declared_attr
 Base = declarative_base()
 
 class TablePrefix(object):
+    """Mixin to provide default table naming"""
+
     PREFIX = "barnacles_"
 
     @declared_attr
@@ -11,13 +13,20 @@ class TablePrefix(object):
         return cls.PREFIX + cls.__name__.lower()
 
 class SurrogateKeyId(object):
+    """Mixin to add an auto increment surrogate primary key (id)"""
+
     __table_args__ = {"sqlite_autoincrement": True}
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, nullable=False)
 
 
 class Tag(Base, TablePrefix, SurrogateKeyId):
-    tag = Column(String(50))
+    tag = Column(String(50), nullable=False)
+
+class VideoTags(Base, TablePrefix, SurrogateKeyId):
+    tag_id = Column(Integer, index=True, nullable=False)
+    video_id = Column(Integer, index=True, nullable=False)
 
 class Video(Base, TablePrefix, SurrogateKeyId):
-    uuid = Column(String(36), unique=True)
+    uuid = Column(String(36), unique=True, nullable=False)
+    path = Column(String(260), nullable=False)
 
