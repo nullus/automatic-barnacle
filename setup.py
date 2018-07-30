34e9492cf6be39fbc2aff97ed1ee1940af88cf53
nullus <dylan.perry@gmail.com>
2018-07-30 22:08:37 +1000

    Translate setuptools config to setup.cfg

--- setup.py
+++ setup.py
@@ -28,22 +28,6 @@
 # POSSIBILITY OF SUCH DAMAGE.
 #
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='barnacles',
-    version='1.1.1',
-    packages=find_packages(),
-    install_requires=[
-        "flask",
-        "rarfile",
-        "sqlalchemy",
-        "alembic",
-    ],
-    package_data={
-        '': ['templates/*.html'],
-    },
-    extras_require={
-        'test': 'waitress',
-    }
-)
+setup()
